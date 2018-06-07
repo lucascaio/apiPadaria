ca9839b4f96837628e43560d1f2125ee012e9f1d
Lucas Matos <matos.lucas@progam.com.br>
2018-06-07 17:17:54 -0300

    Corrigindo api.php

--- routes/api.php
+++ routes/api.php
@@ -18,14 +18,14 @@ Route::group(['middleware'=>'api', 'prefix'=>'auth'], function($router) {
     Route::post('logout','AuthController@logout');
     Route::post('refresh','AuthController@refresh');
     Route::post('me','AuthController@me');
-
-    Route::resource('/anuncio', 'AnuncioController')->only(['index', 'store', 'update', 'destroy']);
-    Route::resource('/fornada', 'FornadaController')->only(['index', 'store', 'update', 'destroy']);
-    Route::resource('/pedido', 'PedidoController')->only(['index', 'store', 'update', 'destroy']);
-    Route::resource('perfil', 'PerfilController')->only(['index', 'store', 'update', 'destroy']);
-    Route::resource('/pessoa', 'PessoaController')->only(['index', 'store', 'update', 'destroy']);
-    Route::resource('/produto', 'ProdutoController')->only(['index', 'store', 'update', 'destroy']);
-    Route::resource('/produto_pedido', 'ProdutoPedidoController')->only(['index', 'store', 'update', 'destroy']);
-    Route::resource('/produto_tipo', 'ProdutoTipoController')->only(['index', 'store', 'update', 'destroy']);
-    Route::resource('/promocao', 'PromocaoController')->only(['index', 'store', 'update', 'destroy']);
 });
+
+Route::resource('/anuncio', 'AnuncioController')->only(['index', 'store', 'update', 'destroy']);
+Route::resource('/fornada', 'FornadaController')->only(['index', 'store', 'update', 'destroy']);
+Route::resource('/pedido', 'PedidoController')->only(['index', 'store', 'update', 'destroy']);
+Route::resource('/perfil', 'PerfilController')->only(['index', 'store', 'update', 'destroy']);
+Route::resource('/pessoa', 'PessoaController')->only(['index', 'store', 'update', 'destroy']);
+Route::resource('/produto', 'ProdutoController')->only(['index', 'store', 'update', 'destroy']);
+Route::resource('/produto_pedido', 'ProdutoPedidoController')->only(['index', 'store', 'update', 'destroy']);
+Route::resource('/produto_tipo', 'ProdutoTipoController')->only(['index', 'store', 'update', 'destroy']);
+Route::resource('/promocao', 'PromocaoController')->only(['index', 'store', 'update', 'destroy']);
