af609eea3a8f191cd1405ff5abf589fce0658fe6
Lucas Matos <matos.lucas@progam.com.br>
2018-06-08 15:56:48 -0300

    Ajustes de fks 4

--- app/ProdutoTipo.php
+++ app/ProdutoTipo.php
@@ -7,6 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class ProdutoTipo extends Model
 {
 
+    protected $table = "tipos";
+
     protected $dates = ['created_at', 'updated_at'];
 
     protected $fillable = ['nome'];
